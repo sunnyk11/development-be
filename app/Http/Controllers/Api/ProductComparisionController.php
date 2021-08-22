0d519909e226c4bade4d813d776763c51152bfb3
iqbal <iqbalrajput1998@gmail.com>
2021-08-22 13:14:02 +0530

    few changes Property

--- app/Http/Controllers/Api/ProductComparisionController.php
+++ app/Http/Controllers/Api/ProductComparisionController.php
@@ -44,7 +44,7 @@ class ProductComparisionController extends Controller
         // user fetch using databse
         $user_id = Auth::user()->id;
         // fetch details property Comparision minimum 5
-         $pro_user_data = Product_Comparision::where(['user_id'=>$user_id,'status'=>1])->get();
+         $pro_user_data = Product_Comparision::where(['user_id'=>$user_id,'status'=>'1'])->get();
         if(count($pro_user_data)<=4){
             // fetch details user product db        
             $pro_comp = Product_Comparision::where(['user_id'=>$user_id,'product_id'=>$request->product_id])->get();
