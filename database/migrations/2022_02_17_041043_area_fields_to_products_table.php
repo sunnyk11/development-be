3a260dd3c510e70ff5d6df8fc32f0740e1649792
iqbal <iqbalrajput1998@gmail.com>
2022-02-17 20:15:34 +0530

    area filed add product table

--- database/migrations/2022_02_17_041043_area_fields_to_products_table.php
+++ database/migrations/2022_02_17_041043_area_fields_to_products_table.php
@@ -14,7 +14,7 @@ class AreaFieldsToProductsTable extends Migration
     public function up()
     {
         Schema::table('products', function (Blueprint $table) {
-            //
+            $table->string('area')->after('area_unit')->nullable();
         });
     }
 
